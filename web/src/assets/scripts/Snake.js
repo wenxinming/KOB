770235f079d512a6644ac05b3ee703e851065ba1
Smart7 <14073355+smart7@user.noreply.gitee.com>
2024-05-04 20:55:04 +0800

    finsh snake move

--- web/src/assets/scripts/Snake.js
+++ web/src/assets/scripts/Snake.js
@@ -31,9 +31,15 @@ export class Snake extends AcGameObject {
         this.direction = d
     }
 
+    check_tail_increasing() {
+        if (this.step <= 10) return true
+        if( this.step % 3 === 1) return true
+        return false
+    }
+
     next_step() {
         const d = this.direction
-        this.next_cell = new Cell(this.cells[0].r + this.dr[d], this.cells[0] + this.dc[d])
+        this.next_cell = new Cell(this.cells[0].r + this.dr[d], this.cells[0].c + this.dc[d]);
         this.direction = -1
         this.status = 'move'
         this.step++
@@ -50,13 +56,27 @@ export class Snake extends AcGameObject {
         const distance = Math.sqrt(dx * dx + dy * dy)
         
         if(distance < this.eps) {
-            this.cell[0] = this.next_cell
+            this.cells[0] = this.next_cell
             this.next_cell = null
             this.status ='idle'
+
+            if(!this.check_tail_increasing()){       
+                this.cells.pop()
+            }
+            
         }else {
             const move_distence = this.speed * this.timedelta / 1000
             this.cells[0].x += move_distence * dx / distance
             this.cells[0].y += move_distence * dy / distance
+
+            if(!this.check_tail_increasing()) {
+                const k = this.cells.length
+                const tail = this.cells[k - 1], tail_target = this.cells[k - 2]
+                const tail_dx = tail_target.x - tail.x
+                const tail_dy = tail_target.y - tail.y
+                tail.x += move_distence * tail_dx / distance
+                tail.y += move_distence * tail_dy / distance
+            }
         }
 
     }
@@ -76,8 +96,18 @@ export class Snake extends AcGameObject {
         ctx.fillStyle = this.color
         for(const cell of this.cells) {
             ctx.beginPath()
-            ctx.arc(cell.x * L, cell.y * L, L / 2, 0, Math.PI * 2)
+            ctx.arc(cell.x * L, cell.y * L, L / 2 * 0.8, 0, Math.PI * 2)
             ctx.fill()
+        }
+
+        for(let i = 1; i < this.cells.length; i++) {
+            const a = this.cells[i - 1], b = this.cells[i]
+            if(Math.abs(a.x - b.x) < this.eps && Math.abs(a.y - b.y) < this.eps) continue;
+            if(Math.abs(a.x - b.x) < this.eps) {
+                ctx.fillRect((a.x - 0.4) * L,Math.min(a.y, b.y) * L, L  * 0.8, Math.abs(a.y - b.y) * L)
+            } else {
+                ctx.fillRect(Math.min(a.x, b.x) * L, (a.y - 0.4) * L, Math.abs(a.x - b.x) * L, L  * 0.8)
+            }
         }
     }
 }
