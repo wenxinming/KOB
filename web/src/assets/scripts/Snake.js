933c794cf2dd0eebd9896e56d27addc92d87a3a1
Smart7 <14073355+smart7@user.noreply.gitee.com>
2024-05-05 21:31:45 +0800

    finsh web snake game

--- web/src/assets/scripts/Snake.js
+++ web/src/assets/scripts/Snake.js
@@ -21,6 +21,22 @@ export class Snake extends AcGameObject {
 
         this.step = 0
         this.eps = 1e-2
+
+        this.eye_direction = 0
+        if(this.id === 1) this.eye_direction = 2  
+
+        this.eye_dx = [
+            [-1,1],
+            [1,1],
+            [1,-1],
+            [-1,-1],
+        ]
+        this.eye_dy = [
+            [-1,-1],
+            [-1,1],
+            [1,1],
+            [1,-1]
+        ]
     }
 
     start() {
@@ -43,10 +59,15 @@ export class Snake extends AcGameObject {
         this.direction = -1
         this.status = 'move'
         this.step++
+        this.eye_direction = d
 
         const k = this.cells.length
         for(let i = k; i > 0; i--) {
             this.cells[i] = JSON.parse(JSON.stringify(this.cells[i - 1]))
+        }
+
+        if(!this.gamemap.check_valid(this.next_cell)) {
+            this.status = 'die'
         }
     }
 
@@ -94,6 +115,11 @@ export class Snake extends AcGameObject {
         const ctx  = this.gamemap.ctx
 
         ctx.fillStyle = this.color
+
+        if(this.status === 'die') {
+            ctx.fillStyle = 'white'
+        }
+
         for(const cell of this.cells) {
             ctx.beginPath()
             ctx.arc(cell.x * L, cell.y * L, L / 2 * 0.8, 0, Math.PI * 2)
@@ -108,6 +134,16 @@ export class Snake extends AcGameObject {
             } else {
                 ctx.fillRect(Math.min(a.x, b.x) * L, (a.y - 0.4) * L, Math.abs(a.x - b.x) * L, L  * 0.8)
             }
+        }
+
+        ctx.fillStyle = 'black'
+        for(let i = 0;i < 2;i++) {
+            const eye_x = (this.cells[0].x + this.eye_dx[this.eye_direction][i] * 0.15) * L 
+            const eye_y = (this.cells[0].y + this.eye_dy[this.eye_direction][i]  * 0.15) * L
+
+            ctx.beginPath()
+            ctx.arc(eye_x, eye_y, L * 0.05, 0, Math.PI * 2)
+            ctx.fill()
         }
     }
 }
