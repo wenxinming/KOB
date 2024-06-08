280a4b76495fad1fec1c3dcf9c757854b05f2e22
Smart7 <14073355+smart7@user.noreply.gitee.com>
2024-06-08 15:04:25 +0800

    finshed pk record database save

---
+++ backend/src/main/java/com/kob/backend/consumer/Cell.java
@@ -0,0 +1,15 @@
+package com.kob.backend.consumer;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author:Smart7 Description:
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cell {
+    int x,y;
+}
